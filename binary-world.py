b3d1d94b3d944f190e62d885571df6cf20ac3cef
Aaron Snoswell <aaronsnoswell@gmail.com>
2018-02-05 14:23:09 +1000

    Initial commit

---
+++ binary-world.py
@@ -0,0 +1,141 @@
+"""
+An implementation of the Binary World Inverse Reinforcement Learning
+benchmark introduced in Wulfmeier, M., Ondruska, P. & Posner, I.
+Maximum Entropy Deep Inverse Reinforcement Learning. (2015).
+"""
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+from contextlib import contextmanager
+
+@contextmanager
+def show_complete_array():
+    """
+    Shows a complete numpy array
+    From https://stackoverflow.com/a/45831462/885287
+    """
+    oldoptions = np.get_printoptions()
+    np.set_printoptions(threshold=np.inf)
+    yield
+    np.set_printoptions(**oldoptions)
+
+
+class BinaryWorld:
+    """
+    An implementation of the Binary World Inverse Reinforcement
+    Learning benchmark introduced in Wulfmeier, M., Ondruska,
+    P. & Posner, I. Maximum Entropy Deep Inverse Reinforcement
+    Learning. (2015).
+    """
+
+    StateColorBlueInt = 0
+    StateColorBlueChar = 'b'
+    StateColorRedInt = 1
+    StateColorRedChar = 'r'
+
+
+    def __init__(self, *, width=32, height=32):
+        """
+        Constructor
+        """
+        
+        self.state_grid = np.random.randint(
+            BinaryWorld.StateColorBlueInt,
+            high=BinaryWorld.StateColorRedInt + 1,
+            size=[height, width]
+        )
+
+
+    def __str__(self):
+        """
+        Get human-friendly string version of class
+        """
+        with show_complete_array():
+            return "BinaryWorld(\n{}\n)".format(
+                "  " + str(self.state_grid)
+                    .replace("\n", "\n  ")
+                    .replace("0", BinaryWorld.StateColorBlueChar)
+                    .replace("1", BinaryWorld.StateColorRedChar)
+            )
+
+
+    def get_reward_from_feature_vector(self, f, *, positive_score_count=4, negative_score_count=5):
+        """
+        Returns the ground truth reward for a given feature vector
+        """
+
+        assert positive_score_count != negative_score_count, "Positive score count ({}) cannot be equal to negative score count".format(positive_score_count)
+
+        num_blue_neighbours = np.sum(np.equal(f, BinaryWorld.StateColorBlueInt))
+        if num_blue_neighbours == positive_score_count:
+            return +1
+        elif num_blue_neighbours == negative_score_count:
+            return -1
+        else:
+            return 0
+
+
+    def get_feature_vector(self, x, y, *, window_size_x=3, window_size_y=3):
+        """
+        Gets the feature vector for a state (x, y)
+        A value of -1 indicates neighbouring states sampled outside the world grid
+        """
+
+        assert (window_size_x % 2) == 1, "Window dimensions must be odd"
+        assert (window_size_y % 2) == 1, "Window dimensions must be odd"
+
+        # Figure out the actual padding size
+        pad_x = window_size_x // 2
+        pad_y = window_size_y // 2
+
+        # Pad the array
+        # TODO ajs 05/Feb/2018 Cache this for performance?
+        state_grid_padded = np.pad(
+            self.state_grid,
+            (pad_x, pad_y),
+            'constant',
+            constant_values=-1
+        )
+
+        # Get the feature vector
+        feature_vector = np.empty(
+            shape=window_size_x*window_size_y,
+            dtype=int
+        )
+
+        ii = 0
+        for yi in range(-pad_y, pad_y+1):
+            y_index = pad_y + y + yi
+            for xi in range(-pad_x, pad_x+1):
+                x_index = pad_x + x + xi
+
+                feature_vector[ii] = state_grid_padded[y_index][x_index]
+                ii += 1
+
+        return feature_vector
+                
+                
+
+
+    def display(self):
+        """
+        Renders the current world state to an image
+        """
+        pass
+
+
+def test_binaryworld():
+    """
+    Tests the BinaryWorld class
+    """
+
+    a = BinaryWorld(width=10, height=10)
+    print(a)
+    f = a.get_feature_vector(3, 3)
+    print(f)
+    print(a.get_reward_from_feature_vector(f))
+
+
+if __name__ == "__main__":
+    test_binaryworld()
