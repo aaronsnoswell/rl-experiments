38e8bd2f1185759ddfbd6c77dac8941db36d3906
Aaron Snoswell <aaronsnoswell@gmail.com>
2018-04-20 00:54:04 +1000

    Working implementation of Large LP IRL!

--- lp_irl.py
+++ lp_irl.py
@@ -226,160 +226,199 @@ def lp_irl(T, gamma, l1, *, Rmax=1.0, method="cvxopt"):
 
     def normalize(vals):
         """
-        normalize to (0, max_val)
-        input:
-        vals: 1d array
+        Helper function to normalize a vector to the range (0, 1)
         """
         min_val = np.min(vals)
         max_val = np.max(vals)
         return (vals - min_val) / (max_val - min_val)
-
     
     # Extract the true optimisation variables and re-scale
-    rewards = Rmax * normalize(res['x'][0:n])
+    rewards = Rmax * normalize(res['x'][0:n]).T
 
     return rewards, res
 
 
-def llp_irl(S0, k, T, phi, *, m=2.0, Rmax=1.0, method="cvxopt"):
+def compute_value_expectation_tensor(sf, M, k, T, phi, N, verbose):
     """
-    Implements Linear Programming IRL for large state spaces by NG and Russell,
-        2000
+    Computes the value expectation tensor VE
 
-    See https://thinkingwires.com/posts/2018-02-13-irl-tutorial-1.html for a
-        good reference.
-    
-    @param S0 - A sampled sub-set of the full state-space S
+    This is an array of shape (d, k-1, M) where VE[:, i, j] is a vector of
+    coefficients that, when multiplied with the alpha vector give the
+    expected difference in value between the expert policy action and the
+    ith action from state s_j
+
+    @param sf - A state factory function that takes no arguments and returns
+        an i.i.d. sample from the MDP state space
+    @param M - The number of sub-samples to draw from the state space |S_0|
+        when estimating the expert's reward function
     @param k - The number of actions |A|
     @param T - A sampling transition function T(s, ai) -> s' encoding a
         stationary deterministic policy. The structure of T must be that the
         0th action T(:, 0) corresponds to a sample from the expert policy, and
         T(:, i), i != 0 corresponds to a sample from the ith non-expert action
         at each state, for some arbitrary but consistent ordering of states
-    @param phi - A vector of d basis functions phi_i(s) mapping from S to the
-        reals
+    @param phi - A vector of d basis functions phi_i(s) mapping from S to real
+        numbers
+    @param N - Number of transition samples to use when computing expectations
+        over the value basis functions
+    @param verbose - If true, progress information will be shown
+
+    @return The value expectation tensor VE. A numpy array of shape
+        (d, k-1, M)
+    """
+
+
+    def expectation(fn, sf, N):
+        """
+        Helper function to estimate an expectation over some function fn(sf())
+
+        @param fn - A function of a single variable that the expectation will
+            be computed over
+        @param sf - A state factory function - takes no variables and returns
+            an i.i.d. sample from the state space
+        @param N - The number of draws to use when estimating the expectation
+
+        @return An estimate of the expectation E[fn(sf())]
+        """
+        state = sf()
+        return sum([fn(sf()) for n in range(N)]) / N
+
+
+    # Measure number of basis functions
+    d = len(phi)
+
+    # Prepare tensor
+    VE_tensor = np.zeros(shape=(d, k-1, M))
+
+    # Draw M initial states from the state space
+    for j in range(M):
+        if verbose: print("{} / {}".format(j, M))
+        s_j = sf()
+
+        # Compute E[phi(s')] where s' is drawn from the expert policy
+        expert_basis_expectations = np.array([
+            expectation(phi[di], lambda: T(s_j, 0), N) for di in range(d)
+        ])
+
+        # Loop over k-1 non-expert actions
+        for i in range(1, k):
+
+            # Compute E[phi(s')] where s' is drawn from the ith non-expert action
+            ith_non_expert_basis_expectations = np.array([
+                expectation(phi[di], lambda: T(s_j, i), N) for di in range(d)
+            ])
+
+            # Compute and store the expectation difference for this initial
+            # state
+            test = expert_basis_expectations - \
+                ith_non_expert_basis_expectations
+
+            VE_tensor[:, i-1, j] = test
+
+    return VE_tensor
+
+
+def llp_irl(sf, M, k, T, phi, *, N=1000, m=2.0, method="cvxopt", verbose=False):
+    """
+    Implements Linear Programming IRL for large state spaces by NG and
+        Russell, 2000
+
+    See https://thinkingwires.com/posts/2018-02-13-irl-tutorial-1.html for a
+        good reference.
+    
+    @param sf - A state factory function that takes no arguments and returns
+        an i.i.d. sample from the MDP state space
+    @param M - The number of sub-samples to draw from the state space |S_0|
+        when estimating the expert's reward function
+    @param k - The number of actions |A|
+    @param T - A sampling transition function T(s, ai) -> s' encoding a
+        stationary deterministic policy. The structure of T must be that the
+        0th action T(:, 0) corresponds to a sample from the expert policy, and
+        T(:, i), i != 0 corresponds to a sample from the ith non-expert action
+        at each state, for some arbitrary but consistent ordering of states
+    @param phi - A vector of d basis functions phi_i(s) mapping from S to real
+        numbers
+    @param N - Number of transition samples to use when computing expectations
+        over the Value basis functions
     @param m - Penalty function coefficient. Ng and Russell find m=2 is robust
-    @param Rmax - Maximum reward value
+        Must be >= 1
     @param method - LP programming method. One of "cvxopt", "scipy-simplex" or
         "scipy-interior-point"
+    @param verbose - If true, progress information will be shown
 
     NB: method == scipy-interior-point depends on scikit-learn>=0.19.1
 
-    @return A vector of d coefficients for the basis functions phi(S)
+    @return A vector of d 'alpha' coefficients for the basis functions phi(S)
         that allows rewards to be computed for a state via the inner product
-        alpha · phi
+        alpha_i · phi
     @return A result object from the optimiser
     """
 
-    # Measure number of sampled states and number of basis functions
-    N = len(S0)
+    # Measure number of basis functions
     d = len(phi)
 
-    # Lambda for the penalty function
-    penalty = lambda x: x if x >= 0 else m*x
+    # Enforce valid penalty function coefficient
+    assert m >= 1, \
+        "Penalty function coefficient must be >= 1, was {}".format(m)
 
-    # Compute E[phi(expert_s') - phi(non_expert_s')] for all non-expert
-    # policies. The phi_tensor[:, i] is the vector
-    # (phi(s_expert') - phi(s_non_expert_i')) / N, where N is the number of
-    # sampled states the expectation is computed over
-    phi_tensor = np.zeros(shape=(d, k-1))
-    for s in S0:
-        expert_new_state = T(s, 0)
-
-        for non_expert_action in range(k - 1):
-            non_expert_new_state = T(s, non_expert_action + 1)
-
-            phi_tensor[:, non_expert_action] += [phi_i(expert_new_state) - phi_i(non_expert_new_state) for phi_i in phi]
-        phi_tensor /= N
-
+    # Precompute the value expectation tensor VE
+    # This is an array of shape (d, k-1, M) where VE[:, i, j] is a vector of
+    # coefficients that, when multiplied with the alpha vector give the
+    # expected difference in value between the expert policy action and the
+    # ith action from state s_j
+    if verbose: print("Computing expectations...")
+    VE_tensor = compute_value_expectation_tensor(sf, M, k, T, phi, N, verbose)
 
     # Formulate the linear programming problem constraints
     # NB: The general form for adding a constraint looks like this
-    # c, A_ub, b_ub, A_eq, b_eq = f(c, A_ub, b_ub, A_eq, b_eq)
+    # c, A_ub, b_ub = f(c, A_ub, b_ub)
+    if verbose: print("Composing LP problem...")
 
 
-    def add_costly_single_step_constraints(c, A_ub, b_ub, A_eq, b_eq):
+    def add_costly_single_step_constraints(c, A_ub, b_ub):
         """
-        Augment the optimisation objective to add the costly-single-step
-        degeneracy heuristic
-        This will add n extra optimisation variables and (k-1) * n extra
-        constraints
-        """
+        Augments the objective and adds constraints to implement the Linear
+        Programming IRL method for large state spaces
 
-        # Extend the optimisation function, adding extra optimisation
-        # variables for every entry in the min{} operator
-        
-        #c_size = c.shape[1]
-        #for non_expert_action in range(k-1):
-        #    c = np.hstack([c, [phi_tensor[:, non_expert_action].T]])
-        #    A_ub = np.hstack([A_ub, np.zeros(shape=[A_ub.shape[0], k])])
-        #    A_eq = np.hstack([A_eq, np.zeros(shape=[A_eq.shape[0], k])])
-
-        # Add constraints to enforce the min{} operator
-        # for ki in range(k-1):
-        #     constraint_row = np.zeros(shape=[A_ub.shape[1]])
-        #     constraint_row[min_offset] = 1
-        #     constraint_row[min_offset + 1 + ki] = -1
-        #     A_ub = np.vstack([A_ub, constraint_row])
-        #     b_ub = np.vstack([b_ub, 0])
-
-        # Add 
-
-        return c, A_ub, b_ub, A_eq, b_eq
-
-
-
-    def add_penalty_function_constraints(c, A_ub, b_ub, A_eq, b_eq, m=m):
-        """
-        Augments the objective function and adds constraints to account for
-        a penalty function p(x) = x if x > 0 else m*x
-        
-        This will add |c| optimisation variables and |c| * 3 constraints
-
-        NB: This function must be called after any LP constraints for it's
-        arguments have been applied
+        This will add M extra variables and 2M*(k-1) constraints
 
         NB: Assumes the true optimisation variables are first in the c vector
         """
 
-        # The penalty function is piecewise linear, with two pieces
-        # Therefore we double the objective function terms, and apply the
-        # appropriate constraint to each piece
-        c_size = c.shape[1]
-        c = np.hstack([c, m * c])
-        A_ub = np.hstack((A_ub, np.zeros(shape=[A_ub.shape[0], c_size])))
-        A_eq = np.hstack([A_eq, np.zeros(shape=[A_eq.shape[0], c_size])])
+        # Step 1: Add the extra optimisation variables for each min{} operator
+        # (one per sampled state)
+        c = np.hstack([np.zeros(shape=(1, d)), np.ones(shape=(1, M))])
+        A_ub = np.hstack([A_ub, np.zeros(shape=(A_ub.shape[0], M))])
 
-        # Add the constraints for the >= 0 part of the penalty function
-        for i in range(d):
-            constraint_row = np.zeros(shape=[A_ub.shape[1]])
-            constraint_row[i] = -1
-            A_ub = np.vstack([A_ub, constraint_row])
-            b_ub = np.vstack([b_ub, 0])
+        # Step 2: Add the constraints
 
-        # Add the constraints for the < 0 part of the penalty function
-        for i in range(d):
-            constraint_row = np.zeros(shape=[A_ub.shape[1]])
-            constraint_row[c_size + i] = 1
-            A_ub = np.vstack([A_ub, constraint_row])
-            b_ub = np.vstack([b_ub, 0])
+        # Loop for each of the starting sampled states s_j
+        for j in range(M):
 
-        # Enforce that the alpha_i's must be equal on each side of the penalty
-        # function
-        for i in range(d):
-            constraint_row = np.zeros(shape=[A_eq.shape[1]])
-            constraint_row[i] = 1
-            constraint_row[c_size + i] = -1
-            A_eq = np.vstack([A_eq, constraint_row])
-            b_eq = np.vstack([b_eq, 0])
+            # Loop over the k-1 non-expert actions
+            for i in range(1, k):
 
-        return c, A_ub, b_ub, A_eq, b_eq
+                # Add two constraints, one for each half of the penalty
+                # function p(x)
+                constraint_row = np.hstack([VE_tensor[:, i-1, j], \
+                    np.zeros(shape=M)])
+                constraint_row[d + j] = -1
+                A_ub = np.vstack((A_ub, constraint_row))
+                b_ub = np.vstack((b_ub, 0))
+
+                constraint_row = np.hstack([m * VE_tensor[:, i-1, j], \
+                    np.zeros(shape=M)])
+                constraint_row[d + j] = -1
+                A_ub = np.vstack((A_ub, constraint_row))
+                b_ub = np.vstack((b_ub, 0))
+
+        return c, A_ub, b_ub
 
 
-    def add_alpha_size_constraints(c, A_ub, b_ub, A_eq, b_eq):
+    def add_alpha_size_constraints(c, A_ub, b_ub):
         """
-        Add constraints for a maximum alpha value of 1
-        This will add d extra constraints
+        Add constraints for a maximum |alpha| value of 1
+        This will add 2 * d extra constraints
 
         NB: Assumes the true optimisation variables are first in the c vector
         """
@@ -388,29 +427,58 @@ def llp_irl(S0, k, T, phi, *, m=2.0, Rmax=1.0, method="cvxopt"):
             constraint_row[i] = 1
             A_ub = np.vstack((A_ub, constraint_row))
             b_ub = np.vstack((b_ub, 1))
-        return c, A_ub, b_ub, A_eq, b_eq
+
+            constraint_row = [0] * A_ub.shape[1]
+            constraint_row[i] = -1
+            A_ub = np.vstack((A_ub, constraint_row))
+            b_ub = np.vstack((b_ub, 1))
+        return c, A_ub, b_ub
 
 
     # Prepare LP constraint matrices
     c = np.zeros(shape=[1, d], dtype=float)
     A_ub = np.zeros(shape=[0, d], dtype=float)
     b_ub = np.zeros(shape=[0, 1])
-    A_eq = np.zeros(shape=[0, d], dtype=float)
-    b_eq = np.zeros(shape=[0, 1])
 
     # Compose LP optimisation problem
-    c, A_ub, b_ub, A_eq, b_eq = add_costly_single_step_constraints(c, A_ub, b_ub, A_eq, b_eq)
-    #c, A_ub, b_ub, A_eq, b_eq = add_penalty_function_constraints(c, A_ub, b_ub, A_eq, b_eq)
-    #c, A_ub, b_ub, A_eq, b_eq = add_alpha_size_constraints(c, A_ub, b_ub, A_eq, b_eq)
+    c, A_ub, b_ub = add_costly_single_step_constraints(c, A_ub, b_ub)
+    c, A_ub, b_ub = add_alpha_size_constraints(c, A_ub, b_ub)
 
-    print(c)
-    print(A_ub)
-    print(b_ub)
-    print(A_eq)
-    print(b_eq)
+    # Show the LP system prior to solving
+    #print(c[0, :])
+    #print(A_ub)
+    #print(b_ub[:, 0])
+    if verbose:
+        print("Number of optimsation variables: {}".format(c.shape[1]))
+        print("Number of constraints: {}".format(A_ub.shape[0]))
 
+    # Solve for a solution
+    if verbose: print("Solving LP problem...")
+    res = None
+    if method == "scipy-simplex":
+        # NB: scipy.optimize.linprog expects a 1d c vector
+        from scipy.optimize import linprog
+        res = linprog(c[0, :], A_ub=A_ub, b_ub=b_ub[:, 0], method="simplex")
 
+    elif method == "scipy-interior-point":
+        # NB: scipy.optimize.linprog expects a 1d c vector
+        from scipy.optimize import linprog
+        res = linprog(c[0, :], A_ub=A_ub, b_ub=b_ub[:, 0],  method="interior-point")
 
+    elif method == "cvxopt":
+        # NB: cvxopt.solvers.lp expects a 1d c vector
+        from cvxopt import matrix, solvers
+        res = solvers.lp(matrix(c[0, :]), matrix(A_ub), matrix(b_ub))
+
+    else:
+
+        raise Exception("Unkown LP method type: {}".format(method))
+        return None
+
+    # Extract the true optimisation variables
+    alpha_vector = res['x'][0:d].T
+
+    return alpha_vector, res
 
 
 
@@ -457,16 +525,18 @@ if __name__ == "__main__":
 
     # Construct an IRL problem from the MountainCar benchmark
     import gym
-    env = gym.make('MountainCar-v0')
+    env = gym.make('MountainCarContinuous-v0')
 
-    # Sample to build state set
-    S0 = []
-    N = 1000
-    for ni in range(N):
-        S0.append(np.random.uniform(
-            low=env.unwrapped.low,
-            high=env.unwrapped.high
-        ))
+    # Lambda that returns i.i.d. samples from state space
+    sf = lambda: [
+            np.random.uniform(env.unwrapped.min_position, \
+                env.unwrapped.max_position),
+            np.random.uniform(-env.unwrapped.max_speed, \
+                env.unwrapped.max_speed)
+        ]
+
+    # Number of states to use for reward function estimation
+    M = 100
 
     # There are three possible actions
     action_set = [0, 1, 2]
@@ -487,7 +557,7 @@ if __name__ == "__main__":
 
         possible_action_set = [expert_action] +  non_expert_action_set
 
-        observation, reward, done, info = env.step(possible_action_set[action_index])
+        observation, reward, done, info = env.step([possible_action_set[action_index]])
         return observation
 
 
@@ -500,62 +570,29 @@ if __name__ == "__main__":
             math.sqrt(2 * math.pi * sigma ** 2)
 
 
-    # Build basis function set
-    d = 4
+    # Build basis function set of size |d|
+    d = 3
     simga = 0.5
     min_pos = env.unwrapped.min_position
     max_pos = env.unwrapped.max_position
     step = (max_pos - min_pos) / d
+    basis_function_positions = np.arange(
+        min_pos + step/2, max_pos + step/2, step
+    )
+
     phi = [
-        lambda s: normal(mu, simga, s[0]) for mu in np.arange(
-            min_pos + step/2, max_pos + step/2, step
-        )
+        lambda s: normal(mu, simga, s[0]) for mu in basis_function_positions
     ]
 
-    llp_irl(S0, k, T, phi)
+    alpha_vector, res = llp_irl(sf, M, k, T, phi, verbose=True)
 
 
-    # # A sample problem for llp_irl
+    # Compose reward function
+    R = lambda s: np.dot(alpha_vector, [phi[i](s) for i in range(len(phi))])[0]
 
-    # # Sample to build a sub-set of state space
-    # n = 100
-    # S0 = np.random.normal(0, 5, n)
+    print(alpha_vector)
 
-    # # Define a sampling transition function
-    # def T(s, a):
-    #     """
-    #     A continuous analog of the 2-state discrete MDP above
-    #     """
-    #     if s < 0:
-    #         # In 'state 1'
-    #         if a == 0:
-    #             # Expert policy from s0
-    #             return np.random.choice([-0.5, 0.5], p=[0.4, 0.6])
-    #         else:
-    #             # Non-expert action from s0
-    #             return np.random.choice([-0.5, 0.5], p=[0.9, 0.1])
-    #     else:
-    #         # In 'state 2'
-    #         if a == 0:
-    #             # Expert policy from s1
-    #             return np.random.choice([-0.5, 0.5], p=[1, 0])
-    #         else:
-    #             # Non-expert action from s1
-    #             return np.random.choice([-0.5, 0.5], p=[1, 0])
+    for sx in np.linspace(env.unwrapped.min_position, env.unwrapped.max_position, 100):
+        print("{}, {}".format(sx, R([sx, 0])))
 
-
-    # # Use a set of normal radial basis functions
-    # phi = [
-    #     lambda s: normal(-0.5, 0.25),
-    #     lambda s: normal(0.5, 0.25)
-    # ]
-
-    # # Try LLP IRL
-    # alpha = rewards, _ = llp_irl(S0, 2, T, phi)
-
-    # # Compose reward function R(s)
-    # R = lambda s: np.inner(alpha, [p(s) for p in phi])
-
-    # # Test reward function
-    # print([R(-0.5), R(0.5)])
 
